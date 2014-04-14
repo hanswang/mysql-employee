a0e5330ef0633ddf1047fe524dd698480f61cb70
hans <hanswang2012@gmail.com>
2014-04-14 00:25:00 +0000

    department class, user collection & DAO

---
+++ public/classes/Department.php
@@ -0,0 +1,194 @@
+<?php
+
+require "base.php";
+global $BASE;
+
+require_once "$BASE/classes/DepartmentDAO.php";
+
+class Department {
+
+    public $dept_id;
+    public $dept_name;
+    public $manager_id;
+    public $manager_startdate;
+    public $manager_enddate;
+    public $employee_count;
+
+    public function __construct($id = false) {
+        if ($id) {
+            $this->manager_id = $id;
+        }
+    }
+
+    public function setManagerId($id) {
+        $this->manager_id = $id;
+    }
+
+    public function load() {
+        if (!isset($this->manager_id)) {
+            trigger_error("Unable to load user without manager id");
+            return false;
+        }
+
+        $raw = DepartmentDAO::loadDepartmentByManagerId($this->manager_id);
+        if (is_array($raw) && count($raw) > 0) {
+            self::setFromArray($raw);
+            return true;
+        }
+        return false;
+    }
+
+    public function setFromArray($raw) {
+        $this->dept_id = $raw['dept_no'];
+        $this->dept_name = $raw['dept_name'];
+        $this->manager_id = $raw['emp_no'];
+        $this->manager_startdate = $raw['from_date'];
+        $this->manager_enddate = $raw['to_date'];
+        $this->employee_count = $raw['employee_count'];
+    }
+
+    public function getDepartmentNo() {
+        return $this->dept_id;
+    }
+
+    public function getEmployeeCount() {
+        return $this->employee_count;
+    }
+}
+
+
+class AllEmployeeView {
+
+    const LIMIT = 100;
+
+    protected $offset;
+    protected $page;
+
+    public $pagecount;
+
+    public function __construct() {
+        $this->offset = 0;
+        $this->page = 0;
+    }
+
+    public function loadPageCount() {
+        $this->pagecount = ceil(DepartmentDAO::getAllCount() / self::LIMIT);
+    }
+
+    public function setPage($page) {
+        if ($page > $this->pagecount - 1) {
+            $this->page = $this->pagecount - 1;
+        } else {
+            $this->page = $page;
+        }
+
+        $this->offset = $page * self::LIMIT;
+
+        return $this->page;
+    }
+
+    public function getPageCount() {
+        return $this->pagecount;
+    }
+
+    public function getEmployeeList() {
+        $users = DepartmentDAO::loadAllUsers($this->offset);
+        return $users;
+    }
+}
+
+
+class DepartmentView extends AllEmployeeView {
+
+    protected $department;
+
+    public function __construct($department) {
+        parent::__construct();
+        $this->department = $department;
+    }
+
+    public function loadPageCount() {
+        $this->pagecount = ceil($this->department->getEmployeeCount() / self::LIMIT);
+    }
+
+    public function getEmployeeList() {
+        $users = DepartmentDAO::loadDepartmentUsersByDeptNo($this->department->getDepartmentNo(), $this->offset);
+        return $users;
+    }
+}
+
+
+class SearchFilterView extends DepartmentView {
+    
+    private $by_firstname;
+    private $by_lastname;
+    private $by_gender;
+    private $by_title;
+    private $by_hiredate;
+
+    public function __construct($department = false) {
+        parent::__construct($department);
+
+        $this->by_firstname = false;
+        $this->by_lastname = false;
+        $this->by_gender = false;
+        $this->by_title = false;
+        $this->by_hiredate = false;
+    }
+
+    public function setFilter($field, $value) {
+        if ($field == 'firstname') {
+            $this->by_firstname = $value;
+        } else if ($field == 'lastname') {
+            $this->by_lastname = $value;
+        } else if ($field == 'gender') {
+            $this->by_gender = $value;
+        } else if ($field == 'title') {
+            $this->by_title = $value;
+        } else if ($field == 'hiredate') {
+            $this->by_hiredate = $value;
+        }
+    }
+
+    public function buildQuery(&$binds) {
+        $sql = ' JOIN titles t ON t.emp_no = e.emp_no '
+            .(($this->department != false) ? ' JOIN dept_emp de ON de.emp_no = e.emp_no ' : '')
+            .' LEFT JOIN titles t2 ON t.emp_no = t2.emp_no AND t2.to_date > t.to_date
+            WHERE t2.title IS NULL '
+            .(($this->department != false) ? ' AND de.dept_no = :dept_no ' : '');
+        $binds[':dept_no'] = $this->department->dept_id;
+
+        if ($this->by_firstname) {
+            $sql .= ' AND e.first_name like :search_fn ';
+            $binds[':search_fn'] = '%'. $this->by_firstname .'%';
+        }
+        if ($this->by_lastname) {
+            $sql .= ' AND e.last_name like :search_ln ';
+            $binds[':search_ln'] = '%'. $this->by_lastname .'%';
+        }
+        if ($this->by_gender) {
+            $sql .= ' AND e.gender = :search_gender ';
+            $binds[':search_gender'] = $this->by_gender;
+        }
+        if ($this->by_title) {
+            $sql .= ' AND t.title = :search_title ';
+            $binds[':search_title'] = $this->by_title;
+        }
+        if ($this->by_hiredate) {
+            $sql .= ' AND e.hire_date > :search_hiredate ';
+            $binds[':search_hiredate'] = $this->by_hiredate;
+        }
+
+        return $sql;
+    }
+
+    public function loadPageCount($sql, $binds) {
+        $this->pagecount = ceil(DepartmentDAO::loadCountByFilteredQuery($sql, $binds) / self::LIMIT);
+    }
+
+    public function getEmployeeList($sql, $binds) {
+        $users = DepartmentDAO::loadEmployeesByFilteredQuery($sql, $binds, $this->offset);
+        return $users;
+    }
+}
+
---
+++ public/classes/DepartmentDAO.php
@@ -0,0 +1,111 @@
+<?php
+
+require "base.php";
+global $BASE;
+require_once "$BASE/lib/functions.php";
+require_once "$BASE/lib/common.php";
+global $dbh;
+
+class DepartmentDAO {
+
+    static public function loadDepartmentByManagerId ($id) {
+        global $dbh;
+        $sth = $dbh->prepare('
+            SELECT d.dept_no, d.dept_name, dm.emp_no, dm.from_date, dm.to_date,
+                COUNT(DISTINCT de.emp_no) AS employee_count
+            FROM departments d
+                JOIN dept_manager dm ON d.dept_no = dm.dept_no
+                JOIN dept_emp de ON de.dept_no = d.dept_no
+            WHERE dm.emp_no = :id
+            ');
+        $sth->execute(array(':id' => $id));
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+        return $row;
+    }
+
+    static public function loadDepartmentUsersByDeptNo ($dept_no, $offset = 0, $limit = 100) {
+        global $dbh;
+        $sth = $dbh->prepare('
+            SELECT e.emp_no, e.first_name, e.last_name, e.gender, e.hire_date, t.title
+            FROM employees e
+                JOIN dept_emp de ON de.emp_no = e.emp_no
+                JOIN titles t ON t.emp_no = e.emp_no
+                LEFT JOIN titles t2 ON t.emp_no = t2.emp_no AND t2.to_date > t.to_date
+            WHERE de.dept_no = :dept_no
+                AND t2.title IS NULL
+            ORDER BY e.emp_no DESC
+            LIMIT :offset, :limit
+            ');
+        $sth->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $sth->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $sth->bindValue(':dept_no', $dept_no);
+        $sth->execute();
+        $row = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        return $row;
+    }
+
+    static public function getAllCount () {
+        global $dbh;
+        $sth = $dbh->prepare('
+            SELECT COUNT(DISTINCT(emp_no)) AS all_count FROM employees
+            ');
+        $sth->execute();
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+        return $row['all_count'];
+    }
+
+    static public function loadAllUsers ($offset = 0, $limit = 100) {
+        global $dbh;
+        $sth = $dbh->prepare('
+            SELECT e.emp_no, e.first_name, e.last_name, e.gender, e.hire_date, t.title
+            FROM employees e
+                JOIN titles t ON t.emp_no = e.emp_no
+                LEFT JOIN titles t2 ON t.emp_no = t2.emp_no AND t2.to_date > t.to_date
+            WHERE t2.title IS NULL
+            ORDER BY e.emp_no DESC
+            LIMIT :offset, :limit
+            ');
+        $sth->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $sth->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        return $row;
+    }
+
+    static public function loadCountByFilteredQuery ($query, $binds) {
+        global $dbh;
+        $combined_query = '
+            SELECT COUNT(DISTINCT(e.emp_no)) AS count FROM employees e' . $query;
+        $sth = $dbh->prepare($combined_query);
+        $sth->execute($binds);
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+        return $row['count'];
+    }
+
+    static public function loadEmployeesByFilteredQuery ($query, $binds, $offset = 0, $limit = 100) {
+
+        global $dbh;
+        $combined_query = '
+            SELECT e.emp_no, e.first_name, e.last_name, e.gender, e.hire_date, t.title
+            FROM employees e' . $query .
+            'ORDER BY e.emp_no DESC LIMIT :offset, :limit
+            ';
+        $sth = $dbh->prepare($combined_query);
+        $sth->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $sth->bindValue(':limit', $limit, PDO::PARAM_INT);
+        foreach($binds as $name => $value) {
+            $sth->bindValue($name, $value);
+        }
+        $sth->execute();
+        $row = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        return $row;
+    }
+
+}
+
